76f911c6e5bf68225b6e8423d6ffa69a9435b952
Cooper Blake <cmblake850@gmail.com>
2025-10-02 17:26:25 -0500

    edit formatting & minor edits

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hw/Vision.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hw/Vision.java
@@ -12,11 +12,11 @@ import org.firstinspires.ftc.vision.apriltag.AprilTagLibrary;
 import org.firstinspires.ftc.vision.apriltag.AprilTagGameDatabase;
 
 
-@TeleOp(name="Vision Test",group="TEST") public class Vision extends LinearOpMode {
+@TeleOp(name="Vision Test",group="TEST")
+public class Vision extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
         AprilTagLibrary currentLibrary = AprilTagGameDatabase.getCurrentGameTagLibrary();
-
         AprilTagProcessor tagProcessor = new AprilTagProcessor.Builder()
                 .setTagLibrary(currentLibrary)
                 .setDrawAxes(true)
@@ -24,19 +24,23 @@ import org.firstinspires.ftc.vision.apriltag.AprilTagGameDatabase;
                 .setDrawTagID(true)
                 .setDrawTagOutline(true)
                 .build();
-
         VisionPortal visionPortal = new VisionPortal.Builder()
                 .setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"))
                 .setCameraResolution(new Size(640, 480))
                 .addProcessor(tagProcessor)
                 .build();
 
-        // visionPortal.start(); // Removed: VisionPortal does not have a start() method in the SDK
-
         waitForStart();
         while(opModeIsActive()) {
-            if(tagProcessor.getDetections().size()>0){
+            if(!tagProcessor.getDetections().isEmpty()){
                 AprilTagDetection tag = tagProcessor.getDetections().get(0);
+                double distance = Math.sqrt(
+                        tag.ftcPose.x * tag.ftcPose.x +
+                        tag.ftcPose.y * tag.ftcPose.y +
+                        tag.ftcPose.z * tag.ftcPose.z
+                );
+                double elevation = Math.toDegrees(Math.atan2(tag.ftcPose.y, tag.ftcPose.z));
+
                 telemetry.addData("Detected Tag ID", tag.id);
                 telemetry.addData("Left/Right X (in)", tag.ftcPose.x);
                 telemetry.addData("Up/Down Y (in)", tag.ftcPose.y);
@@ -44,13 +48,6 @@ import org.firstinspires.ftc.vision.apriltag.AprilTagGameDatabase;
                 telemetry.addData("Yaw (deg)", tag.ftcPose.yaw);
                 telemetry.addData("Pitch (deg)", tag.ftcPose.pitch);
                 telemetry.addData("Roll (deg)", tag.ftcPose.roll);
-
-                double distance = Math.sqrt(
-                        tag.ftcPose.x * tag.ftcPose.x +
-                        tag.ftcPose.y * tag.ftcPose.y +
-                        tag.ftcPose.z * tag.ftcPose.z
-                );
-                double elevation = Math.toDegrees(Math.atan2(tag.ftcPose.y, tag.ftcPose.z));
 
                 telemetry.addData("Distance (in)", distance);
                 telemetry.addData("Elevation (deg)", elevation);
