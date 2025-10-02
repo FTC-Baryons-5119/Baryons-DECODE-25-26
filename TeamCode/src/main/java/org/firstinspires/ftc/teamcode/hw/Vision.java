dff3e205bd1450d332b46ed36f5ed2cd5b759d2d
Cooper Blake <cmblake850@gmail.com>
2025-10-02 17:59:05 -0500

    restructure Vision class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hw/Vision.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hw/Vision.java
@@ -3,7 +3,9 @@ import android.util.Size;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
@@ -11,53 +13,72 @@ import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.apriltag.AprilTagLibrary;
 import org.firstinspires.ftc.vision.apriltag.AprilTagGameDatabase;
 
+import java.util.List;
+import java.util.Optional;
 
-@TeleOp(name="Vision Test",group="TEST")
-public class Vision extends LinearOpMode {
-    @Override
-    public void runOpMode() throws InterruptedException {
-        AprilTagLibrary currentLibrary = AprilTagGameDatabase.getCurrentGameTagLibrary();
-        AprilTagProcessor tagProcessor = new AprilTagProcessor.Builder()
+public class Vision {
+    private AprilTagLibrary currentLibrary;
+    private AprilTagProcessor tagProcessor;
+    private VisionPortal visionPortal;
+
+    public Vision(HardwareMap hardwareMap) {
+        currentLibrary = AprilTagGameDatabase.getCurrentGameTagLibrary();
+        tagProcessor = new AprilTagProcessor.Builder()
                 .setTagLibrary(currentLibrary)
                 .setDrawAxes(true)
                 .setDrawCubeProjection(true)
                 .setDrawTagID(true)
                 .setDrawTagOutline(true)
                 .build();
-        VisionPortal visionPortal = new VisionPortal.Builder()
+        // even though visionPortal isn't referenced here, it is still used by the AprilTagProcessor
+        visionPortal = new VisionPortal.Builder()
                 .setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"))
                 .setCameraResolution(new Size(640, 480))
                 .addProcessor(tagProcessor)
                 .build();
+    }
 
-        waitForStart();
-        while(opModeIsActive()) {
-            if(!tagProcessor.getDetections().isEmpty()){
-                AprilTagDetection tag = tagProcessor.getDetections().get(0);
-                double distance = Math.sqrt(
-                        tag.ftcPose.x * tag.ftcPose.x +
+    // note - list could be empty
+    public List<AprilTagDetection> getTags() {
+        return tagProcessor.getDetections();
+    }
+
+    // java doesn't have anonymous datastructures :(
+    private static class tagPoseData {
+        double distance;
+        double elevation;
+    }
+    public tagPoseData getTagPoseData(AprilTagDetection tag) {
+        tagPoseData data = new tagPoseData();
+        data.distance = Math.sqrt(
+                tag.ftcPose.x * tag.ftcPose.x +
                         tag.ftcPose.y * tag.ftcPose.y +
                         tag.ftcPose.z * tag.ftcPose.z
-                );
-                double elevation = Math.toDegrees(Math.atan2(tag.ftcPose.y, tag.ftcPose.z));
+        );
+        data.elevation = Math.toDegrees(Math.atan2(tag.ftcPose.y, tag.ftcPose.z));
+        return data;
+    }
 
-                telemetry.addData("Detected Tag ID", tag.id);
-                telemetry.addData("Left/Right X (in)", tag.ftcPose.x);
-                telemetry.addData("Up/Down Y (in)", tag.ftcPose.y);
-                telemetry.addData("Forward Z (in)", tag.ftcPose.z);
-                telemetry.addData("Yaw (deg)", tag.ftcPose.yaw);
-                telemetry.addData("Pitch (deg)", tag.ftcPose.pitch);
-                telemetry.addData("Roll (deg)", tag.ftcPose.roll);
+    public void getTagTelemetry(Telemetry telemetry, List<AprilTagDetection> tags) {
+        for (int i = 0; i < tags.size(); i++) {
+            AprilTagDetection tag = tags.get(i);
+            double distance = Math.sqrt(
+                    tag.ftcPose.x * tag.ftcPose.x +
+                            tag.ftcPose.y * tag.ftcPose.y +
+                            tag.ftcPose.z * tag.ftcPose.z
+            );
+            double elevation = Math.toDegrees(Math.atan2(tag.ftcPose.y, tag.ftcPose.z));
 
-                telemetry.addData("Distance (in)", distance);
-                telemetry.addData("Elevation (deg)", elevation);
-            } else {
-                telemetry.addData("Status", "No tag detected");
-            }
-            telemetry.update();
+            telemetry.addData("Detected Tag ID", tag.id);
+            telemetry.addData("Left/Right X (in)", tag.ftcPose.x);
+            telemetry.addData("Up/Down Y (in)", tag.ftcPose.y);
+            telemetry.addData("Forward Z (in)", tag.ftcPose.z);
+            telemetry.addData("Yaw (deg)", tag.ftcPose.yaw);
+            telemetry.addData("Pitch (deg)", tag.ftcPose.pitch);
+            telemetry.addData("Roll (deg)", tag.ftcPose.roll);
+
+            telemetry.addData("Distance (in)", distance);
+            telemetry.addData("Elevation (deg)", elevation);
         }
-
-
-
     }
 }
